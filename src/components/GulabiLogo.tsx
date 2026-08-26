0e974113fc95c5ac697bec098d81af2621398d6f
Sudarshan Killekar <sudarshankillekar1998@gmail.com>
2026-08-26 23:12:37 +0530

    Refine logo placement

--- src/components/GulabiLogo.tsx
+++ src/components/GulabiLogo.tsx
@@ -6,18 +6,17 @@ type GulabiLogoProps = {
 export function GulabiLogo({ className = "", markOnly = false }: GulabiLogoProps) {
   return (
     <span className={`gulabi-logo ${className}`.trim()}>
-      <svg viewBox="0 0 138 104" aria-hidden="true" focusable="false">
-        <path className="gulabi-logo-heart" d="M9 55c-9-12 9-22 17-7 8-15 26-5 17 7-7 10-17 17-17 17S16 65 9 55Z" />
-        <path className="gulabi-logo-thread" d="M26 72c15 10 37 6 50-8" />
-        <circle className="gulabi-logo-ball" cx="84" cy="55" r="34" />
-        <path className="gulabi-logo-yarn" d="M61 41c15-9 32-17 52-27" />
-        <path className="gulabi-logo-yarn" d="M54 57c20-12 42-23 66-35" />
-        <path className="gulabi-logo-yarn" d="M60 72c19-10 39-21 60-32" />
-        <path className="gulabi-logo-yarn" d="M73 25c15 18 29 37 42 59" />
-        <path className="gulabi-logo-yarn" d="M52 55c16 16 31 32 44 50" />
-        <path className="gulabi-logo-needle" d="M57 93 124 12" />
-        <path className="gulabi-logo-needle" d="M124 12l8-3" />
-        <circle className="gulabi-logo-pin" cx="124" cy="12" r="5" />
+      <svg viewBox="0 0 116 86" aria-hidden="true" focusable="false">
+        <path className="gulabi-logo-heart" d="M8 38c-8-11 8-20 15-7 7-13 23-4 15 7-5 8-15 14-15 14S13 46 8 38Z" />
+        <path className="gulabi-logo-thread" d="M22 52c10 7 23 6 34-2" />
+        <circle className="gulabi-logo-ball" cx="65" cy="40" r="30" />
+        <path className="gulabi-logo-yarn" d="M44 34c14-8 29-15 46-23" />
+        <path className="gulabi-logo-yarn" d="M40 48c16-9 34-17 53-25" />
+        <path className="gulabi-logo-yarn" d="M47 59c14-7 29-15 45-23" />
+        <path className="gulabi-logo-yarn" d="M53 15c12 13 24 27 36 43" />
+        <path className="gulabi-logo-yarn" d="M41 41c13 12 25 24 35 36" />
+        <path className="gulabi-logo-needle" d="M34 72 96 4" />
+        <path className="gulabi-logo-needle" d="M96 4 104 1" />
       </svg>
       {!markOnly && (
         <span className="gulabi-logo-copy">
